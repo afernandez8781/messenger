33c343bf21200574c5c69b65b4c02db53224b1e5
Alfredo Fernandez <afernandez8781@hotmail.com>
2018-08-21 22:35:43 -0400

    Login UI advances

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+<b-container>
+    <b-row align-h="center">
+        <b-col cols="8">
+
+            <b-card title="Inicio de sesión" style="box-shadow: 0px 0px 10px rgba(0,0,0,0.3);">
+                <b-alert show>
+                    Por favor íngresa tus datos:
+                </b-alert>
+                
+
+                <b-form method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+
+
+                    <b-form-group
+                        label="Correo electrónico" label-for="email"
+                        description="Nunca compartiremos tu correo. Está seguro con nosotros.">
+
+                        <b-form-input id="email"
+                            type="email" name="email"
+                            value="{{ old('email') }}" required autofocus
+                            placeholder="example@hotmail.com">
+                        </b-form-input>
+
+                    </b-form-group>
+
+                    <b-form-group
+                        label="Contraseña" label-for="password">
+
+                        <b-form-input type="password"
+                            name="password" name="password"
+                            value="{{ old('password') }}" required>
+                        </b-form-input>
+
+                    </b-form-group>
+                    
+                    <b-form-group>
+                        <b-form-checkbox 
+                            name="remember"
+                            {{ old('remember') ? 'checked="true"' : '' }}>
+                            Recordar sesión
+                        </b-form-checkbox>
+                    </b-form-group>
+
+                        <b-button type="submit" variant="primary">
+                            Ingresar
+                        </b-button>
+
+                        <b-button href="{{ route('password.request') }}" variant="link">
+                            ¿Olvidaste tu contraseña?
+                        </b-button>
+
+
+                </b-form>
+
+            </b-card>
+
+        </b-col>
+    </b-row>
+</b-container>
+@endsection
